7873ce7e7286df35125a690af4ebaef3ae67247d
Your Name <ndeyefatoumbaye664@gmail.com>
2026-08-30 20:08:57 +0200

    Fix scroll to top navigation

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import AdminLogin from "./admin/AdminLogin";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ScrollToTop from "./components/ScrollToTop";
+
 import Contact from "./Pages/Contact";
 import Home from "./Pages/Home";
 import Shop from "./Pages/Shop";
@@ -12,7 +13,6 @@ import Cart from "./Pages/Cart";
 import Checkout from "./Pages/Checkout";
 import Favorites from "./Pages/Favorites";
 
-
 import AdminLayout from "./admin/AdminLayout";
 import Dashboard from "./admin/Dashboard";
 import ProductsAdmin from "./admin/ProductsAdmin";
@@ -20,177 +20,100 @@ import AddProduct from "./admin/Addproduct";
 import EditProduct from "./admin/EditProduct";
 import OrdersAdmin from "./admin/OrdersAdmin";
 
-
-
-const App = ()=>{
-
-
-return (
-
-<BrowserRouter>
-
-
-<Navbar />
-
-
-<Routes>
-
-<Route
-  path="/admin/login"
-  element={<AdminLogin />}
-/>
-
- <ScrollToTop />
-<Route
-
-path="/contact"
-
-element={<Contact />}
-
-/>
-{/* CLIENT */}
-
-
-<Route
-
-path="/"
-
-element={<Home />}
-
-/>
-
-
-<Route
-
-path="/shop"
-
-element={<Shop />}
-
-/>
-
-
-<Route
-
-path="/product/:id"
-
-element={<ProductDetails />}
-
-/>
-
-
-<Route
-
-path="/cart"
-
-element={<Cart />}
-
-/>
-
-
-<Route
-
-path="/checkout"
-
-element={<Checkout />}
-
-/>
-
-
-<Route
-
-path="/favorites"
-
-element={<Favorites />}
-
-/>
-
-
-
-
-
-
-
-{/* ADMIN */}
-
-
-<Route
-
-path="/admin"
-
-element={<AdminLayout />}
-
->
-
-
-<Route
-
-index
-
-element={<Dashboard />}
-
-/>
-
-
-
-<Route
-
-path="products"
-
-element={<ProductsAdmin />}
-
-/>
-
-
-
-<Route
-
-path="products/add"
-
-element={<AddProduct />}
-
-/>
-
-
-
-<Route
-
-path="products/edit/:id"
-
-element={<EditProduct />}
-
-/>
-
-
-
-<Route
-
-path="orders"
-
-element={<OrdersAdmin />}
-
-/>
-
-
-</Route>
-
-
-
-</Routes>
-
-
-
-
-
-<Footer />
-
-
-</BrowserRouter>
-
-
-);
-
-
+const App = () => {
+  return (
+    <BrowserRouter>
+
+      {/* Remonte automatiquement en haut à chaque changement de page */}
+      <ScrollToTop />
+
+      <Navbar />
+
+      <Routes>
+
+        {/* ================= CLIENT ================= */}
+
+        <Route
+          path="/"
+          element={<Home />}
+        />
+
+        <Route
+          path="/shop"
+          element={<Shop />}
+        />
+
+        <Route
+          path="/product/:id"
+          element={<ProductDetails />}
+        />
+
+        <Route
+          path="/cart"
+          element={<Cart />}
+        />
+
+        <Route
+          path="/checkout"
+          element={<Checkout />}
+        />
+
+        <Route
+          path="/favorites"
+          element={<Favorites />}
+        />
+
+        <Route
+          path="/contact"
+          element={<Contact />}
+        />
+
+        {/* ================= ADMIN ================= */}
+
+        <Route
+          path="/admin/login"
+          element={<AdminLogin />}
+        />
+
+        <Route
+          path="/admin"
+          element={<AdminLayout />}
+        >
+
+          <Route
+            index
+            element={<Dashboard />}
+          />
+
+          <Route
+            path="products"
+            element={<ProductsAdmin />}
+          />
+
+          <Route
+            path="products/add"
+            element={<AddProduct />}
+          />
+
+          <Route
+            path="products/edit/:id"
+            element={<EditProduct />}
+          />
+
+          <Route
+            path="orders"
+            element={<OrdersAdmin />}
+          />
+
+        </Route>
+
+      </Routes>
+
+      <Footer />
+
+    </BrowserRouter>
+  );
 };
 
-
 export default App;
+
